b2184d2c95ec46eae84cd2605471b9be347fd13b
Azurene <98633770+Azurene@users.noreply.github.com>
2022-06-05 21:27:19 -0700

    updated models and started on category-routes

--- models/Product.js
+++ models/Product.js
@@ -19,6 +19,27 @@ Product.init(
     product_name: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    price: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
