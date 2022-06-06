ddf29294f3855afc0b0ede3b2366772586e72284
Azurene <98633770+Azurene@users.noreply.github.com>
2022-06-05 23:29:42 -0700

    added product routes and stuff

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -11,16 +11,47 @@ router.get('/', (req, res) => {
     include: [
       {
         model: Category,
-        attributes
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        through: ProductTag,
+        attributes: ['tag_name']
       }
     ]
   })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name'] 
+      },
+      {
+        model: Tag,
+        through: ProductTag,
+        attributes: ['tag_name']
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
